f2076c63892051fb7e49aaaaa06ec7b1cbeabc78
Satyaveerjaligama <satyaveerjaligama26@gmail.com>
2023-11-05 11:40:46 +0530

    added type writer code

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -1,12 +1,23 @@
-import { Box, Typography, Button } from "@mui/material";
-import DownloadIcon from '@mui/icons-material/Download';
+import { Box, Typography } from "@mui/material";
+import Typewriter from "typewriter-effect";
 
 const Intro = () => {
     return (
         <Box id="Home" sx={{paddingTop: "100px"}}>
             <Typography variant="h3" className="font-young-serif">Hey, I'm <span style={{color: "#C887C5"}}>{`<SatyaveerJaligama />`}</span></Typography>
-            <Typography variant="h5" className="font-young-serif">Frontend Developer</Typography>
-            {/* <Button download href="/logo192.png" variant="contained" startIcon={<DownloadIcon />} sx={{textTransform: "none", borderRadius: "10px"}}>Download Resume</Button> */}
+            <Typography variant="h5" className="font-young-serif">
+                <Typewriter
+                    loop
+                    onInit={(typewriter) => {
+                        typewriter
+                            .typeString("Frontend Developer")
+                            .pauseFor(1000)
+                            .deleteAll()
+                            .typeString("Frontend Developer")
+                            .start();
+                    }}
+                />
+            </Typography>
         </Box>
     )
 }
