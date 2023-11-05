cda8fbc1deff51b3cbc8ac20964377b05e91dd00
Satyaveerjaligama <satyaveerjaligama26@gmail.com>
2023-11-05 15:23:06 +0530

    Mobile responsive - education css changes

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,5 +1,6 @@
 import { Typography } from "@mui/material";
 import "../Styles/ExperienceCard.css";
+import "../Styles/Education.css";
 import {
   Timeline,
   TimelineItem,
@@ -12,7 +13,7 @@ import {
 
 const Education = () => {
   return (
-    <div id="Education" style={{ paddingTop: "75px" }} className="educationMain">
+    <div id="Education" className="educationMain">
       <Typography variant="h4" className="educationTitle font-young-serif">
         Education
       </Typography>
@@ -27,21 +28,21 @@ const Education = () => {
               <TimelineConnector />
             </TimelineSeparator>
             <TimelineContent>
-              <Typography className="font-signika-negative font-size-20 text-decoration-underline">Vellore Institute Of Technology, Vellore (VIT University)</Typography>
+              <Typography className="font-young-serif">Vellore Institute Of Technology, Vellore (VIT University)</Typography>
               <Typography className="font-signika-negative">2017 - 2022</Typography>
               <Typography className="font-signika-negative">CGPA - 8.72</Typography>
             </TimelineContent>
           </TimelineItem>
           <TimelineItem>
             <TimelineOppositeContent>
-              <Typography className="font-young-serif">Intermediate (11<sup>th</sup>, 12<sup>th</sup>)</Typography>
+              <Typography className="font-young-serif">Intermediate (11<sup>th</sup>,12<sup>th</sup>)</Typography>
             </TimelineOppositeContent>
             <TimelineSeparator>
               <TimelineDot/>
               <TimelineConnector />
             </TimelineSeparator>
             <TimelineContent>
-              <Typography className="font-signika-negative font-size-20 text-decoration-underline">Sri Gayatri Educational Institution, Hyderabad</Typography>
+              <Typography className="font-young-serif">Sri Gayatri Educational Institution, Hyderabad</Typography>
               <Typography className="font-signika-negative">2015 - 2017</Typography>
               <Typography className="font-signika-negative">Percentage - 95.2</Typography>
             </TimelineContent>
@@ -54,7 +55,7 @@ const Education = () => {
               <TimelineDot/>
             </TimelineSeparator>
             <TimelineContent>
-              <Typography className="font-signika-negative font-size-20 text-decoration-underline">Sri Chaitanya Educational Institution, Hyderabad</Typography>
+              <Typography className="font-young-serif">Sri Chaitanya Educational Institution, Hyderabad</Typography>
               <Typography className="font-signika-negative">2014 - 2015</Typography>
               <Typography className="font-signika-negative">GPA - 8.5</Typography>
             </TimelineContent>
