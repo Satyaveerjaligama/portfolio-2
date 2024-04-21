3af34f72dc3c879c552af2dbc02803d97cadd7b9
Satyaveerjaligama <satyaveerjaligama26@gmail.com>
2024-04-22 00:11:03 +0530

    Added phone number in contact details

--- src/Components/ContactCard.jsx
+++ src/Components/ContactCard.jsx
@@ -1,5 +1,5 @@
 import { Box, Grid, Link, Typography } from "@mui/material";
-import { LinkedIn, WhatsApp, EmailOutlined, GitHub } from "@mui/icons-material";
+import { LinkedIn, WhatsApp, EmailOutlined, GitHub, LocalPhoneOutlined } from "@mui/icons-material";
 import "../Styles/ContactCard.css";
 
 const ContactCard = () => {
@@ -7,23 +7,28 @@ const ContactCard = () => {
         <Box id="Contact" className="contactMain">
             <Typography variant="h6" className="font-young-serif text-decoration-underline">Contact Me</Typography>
             <Grid container>
-                <Grid item xs={6} sm={3}>
+                <Grid item xs={6} sm={2.4}>
                     <Link href="https://github.com/Satyaveerjaligama" target="_blank">
                         <GitHub className="contactIcons"/>
                     </Link>
                 </Grid>
-                <Grid item xs={6} sm={3}>
+                <Grid item xs={6} sm={2.4}>
                     <Link href="https://www.linkedin.com/in/satyaveer-jaligama" target="_blank">
                         <LinkedIn className="contactIcons"/>
                     </Link>
                 </Grid>
-                <Grid item xs={6} sm={3}>
+                <Grid item xs={6} sm={2.4}>
                     <Link href="mailto:satyaveerjaligama13@gmail.com" target="_blank">
                         <EmailOutlined className="contactIcons"/>
                     </Link>
                 </Grid>
-                <Grid item xs={6} sm={3}>
-                    <Link href="https://wa.me/qr/FB2UCT6ZJPQKL1" target="_blank">
+                <Grid item xs={6} sm={2.4}>
+                    <Link href="tel:9182313086" target="_blank">
+                        <LocalPhoneOutlined className="contactIcons"/>
+                    </Link>
+                </Grid>
+                <Grid item xs={6} sm={2.4}>
+                    <Link href="https://wa.me/qr/OSUEJTTLXWGUH1" target="_blank">
                         <WhatsApp className="contactIcons"/>
                     </Link>
                 </Grid>
